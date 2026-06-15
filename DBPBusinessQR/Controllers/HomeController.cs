138b04c13920610776a971003ee9ee201173cf1e
cdepurificacion <cdepurificacion@CDEPURIFICACION>
2026-06-15 14:01:06 +0800

    DBP Digital Business Card - QR flip card with contact save

---
+++ DBPBusinessQR/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using DBPBusinessQR.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace DBPBusinessQR.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Card()
+        {
+            return View();
+        }
+
+        public IActionResult DownloadContact()
+        {
+            // VCF format — phone will offer to save this to Contacts with name and number pre-filled
+            string vcf =
+                "BEGIN:VCARD\r\n" +
+                "VERSION:3.0\r\n" +
+                "FN:Chelsea De Purificacion\r\n" +
+                "N:De Purificacion;Chelsea;;;\r\n" +
+                "ORG:Development Bank of the Philippines\r\n" +
+                "TITLE:Software Engineer / Cybersecurity Analyst\r\n" +
+                "TEL;TYPE=CELL:+639614475634\r\n" +
+                "EMAIL:cdepurificacion@dbp.ph\r\n" +
+                "URL:https://github.com/MsCereal\r\n" +
+                "END:VCARD\r\n";
+
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(vcf);
+
+            return File(bytes, "text/vcard", "Chelsea-DBP.vcf");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            });
+        }
+    }
+}
---
+++ DBPBusinessQR/Controllers/QRController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using QRCoder;
+
+namespace DBPBusinessQR.Controllers
+{
+    public class QRController : Controller
+    {
+        // /QR/Show  — display the QR code on screen (use this to scan from your phone)
+        public IActionResult Show()
+        {
+            return View();
+        }
+
+        // /QR/Generate  — returns the raw QR PNG image
+        public IActionResult Generate()
+        {
+            // Build an absolute URL using the current request so it works on any host/port
+            string url = $"{Request.Scheme}://{Request.Host}/Home/Index";
+
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+
+            PngByteQRCode qrCode = new PngByteQRCode(qrCodeData);
+            byte[] qrBytes = qrCode.GetGraphic(20);
+
+            return File(qrBytes, "image/png");
+        }
+    }
+}
